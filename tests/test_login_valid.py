61c32b19425fd0943dad0c0d2edfd026cf48137b
pedrocoelho.tester <86770446+pedrokoelho@users.noreply.github.com>
2024-05-01 14:20:37 +0100

    Added the test valid login assertions

--- tests/test_login_valid.py
+++ tests/test_login_valid.py
@@ -11,6 +11,7 @@ Test case 1: Positive LogIn test
 """
 import pytest
 from pages.login_page import LoginPage
+from pages.logged_in_page import LoggedInPage
 
 class TestLoginValid:
 
@@ -18,8 +19,9 @@ class TestLoginValid:
     @pytest.mark.valid
     def test_login_valid(self, driver):
 
-        # instantiating the class page
+        # instantiating the class pages
         login_page = LoginPage(driver)
+        logged_in_page = LoggedInPage(driver)
 
         # 1. Open page
         login_page.open()
@@ -28,5 +30,8 @@ class TestLoginValid:
         # 4. Push Submit button
         login_page.execute_login('student', 'Password123')
         # 5. Verify new page URL contains practicetestautomation.com/logged-in-successfully/
-        # 6. Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+        assert logged_in_page.expected_url == logged_in_page.current_url, 'Captured URL does not match Expected URL'
+        # 6. Verify new page contains expected text ('Logged In Successfully')
+        assert logged_in_page.header == 'Logged In Successfully', 'Captured text does not match expected text'
         # 7. Verify button Log out is displayed on the new page
+        assert logged_in_page.is_loggout_btn_displayed(), 'Log Out button was not displayed'
